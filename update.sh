2c8a1165f493bd3b30da2cbab7e6cdc419b6b3b1
nercone-dev <nercone@nercone.dev>
2026-06-27 04:16:27 +0900

    --

--- update.sh
+++ update.sh
@@ -42,9 +42,9 @@ echo "Found: zstd-nginx-module ${ZSTD_NGINX_MODULE_COMMIT}"
 # Version Check: Debian Package List
 echo "> GET Package List Checksum"
 
-MAIN_PKG_HASH=$(curl -fsSL "http://deb.debian.org/debian/dists/bookworm/Release" awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
+MAIN_PKG_HASH=$(curl -fsSL "http://deb.debian.org/debian/dists/bookworm/Release" | awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
 
-SEC_PKG_HASH=$(curl -fsSL "https://security.debian.org/debian-security/dists/bookworm-security/Release" awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
+SEC_PKG_HASH=$(curl -fsSL "https://security.debian.org/debian-security/dists/bookworm-security/Release" | awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
 
 DEBIAN_PACKAGES_HASH="${MAIN_PKG_HASH:0:16}-${SEC_PKG_HASH:0:16}"
 
