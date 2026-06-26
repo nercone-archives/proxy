c116798e503336e5c19c6d5ecd5bdc57812e7a32
nercone-dev <nercone@nercone.dev>
2026-06-27 03:58:49 +0900

    --

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ echo "> RUN git pull"
 
 git pull
 
-echo "> GET OpenSSL 3.y.z"
+echo "> GET OpenSSL x.y.z"
 
 OPENSSL_VERSION=$(curl -fsSL "https://api.github.com/repos/openssl/openssl/releases?per_page=100" \
     | grep -o '"tag_name": *"openssl-[^"]*"' \
     | sed 's/.*openssl-\([^"]*\)".*/\1/' \
-    | grep -E '^3\.[0-9]+\.[0-9]+$' \
+    | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' \
     | sort -V \
     | tail -1)
 
