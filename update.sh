8c632992a535050980b83a1b1fded0e9d8bdad63
nercone-dev <nercone@nercone.dev>
2026-06-27 04:55:16 +0900

    --

--- update.sh
+++ update.sh
@@ -6,6 +6,19 @@ echo "> RUN git pull"
 
 git pull
 
+# Version Check: Debian Package List
+echo "> VERSION Debian Package List"
+
+MAIN_RELEASE=$(curl -fsSL "http://deb.debian.org/debian/dists/bookworm/Release")
+MAIN_PKG_HASH=$(echo "${MAIN_RELEASE}" | awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
+
+SEC_RELEASE=$(curl -fsSL "https://security.debian.org/debian-security/dists/bookworm-security/Release")
+SEC_PKG_HASH=$(echo "${SEC_RELEASE}" | awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
+
+DEBIAN_PACKAGES_HASH="${MAIN_PKG_HASH:0:16}-${SEC_PKG_HASH:0:16}"
+
+echo "Debian Package List ${DEBIAN_PACKAGES_HASH}"
+
 # Version Check: OpenSSL
 echo "> VERSION OpenSSL"
 
@@ -39,27 +52,14 @@ ZSTD_NGINX_MODULE_COMMIT=$(curl -fsSL "https://api.github.com/repos/tokers/zstd-
 
 echo "zstd-nginx-module ${ZSTD_NGINX_MODULE_COMMIT}"
 
-# Version Check: Debian Package List
-echo "> VERSION Debian Package List"
-
-MAIN_RELEASE=$(curl -fsSL "http://deb.debian.org/debian/dists/bookworm/Release")
-MAIN_PKG_HASH=$(echo "${MAIN_RELEASE}" | awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
-
-SEC_RELEASE=$(curl -fsSL "https://security.debian.org/debian-security/dists/bookworm-security/Release")
-SEC_PKG_HASH=$(echo "${SEC_RELEASE}" | awk '/^SHA256:/{in_sha=1; next} in_sha && / main\/binary-amd64\/Packages$/{print $1; exit}')
-
-DEBIAN_PACKAGES_HASH="${MAIN_PKG_HASH:0:16}-${SEC_PKG_HASH:0:16}"
-
-echo "Debian Package List ${DEBIAN_PACKAGES_HASH}"
-
 # Build
 echo "> RUN docker compose build"
 
 docker compose build \
+    --build-arg DEBIAN_PACKAGES_HASH="${DEBIAN_PACKAGES_HASH}" \
     --build-arg OPENSSL_VERSION="${OPENSSL_VERSION}" \
     --build-arg NGINX_VERSION="${NGINX_VERSION}" \
-    --build-arg ZSTD_NGINX_MODULE_COMMIT="${ZSTD_NGINX_MODULE_COMMIT}" \
-    --build-arg DEBIAN_PACKAGES_HASH="${DEBIAN_PACKAGES_HASH}"
+    --build-arg ZSTD_NGINX_MODULE_COMMIT="${ZSTD_NGINX_MODULE_COMMIT}"
 
 # Start
 echo "> RUN docker compose up -d"
